dee80f5103d53b3c856a4e1437438fb010adf542
Kevin A Dobbin <94518887+KEVINADOBBIN@users.noreply.github.com>
2026-01-08 12:34:12 +0530

    Add files via upload

---
+++ system_configuration.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "--------------------------------------------"
+echo "REGISTER NUMBER: 408CS15015"
+echo "EXPERIMENT: SYSTEM CONFIGURATION DETAILS"
+echo "--------------------------------------------"
+
+echo
+echo "To show various system configuration"
+
+echo
+echo "A) Display the currently logged user and log name"
+echo "Current logged user: $USER"
+echo "Login name: $LOGNAME"
+
+echo
+echo "B) Display the current shell"
+echo "Current shell is: $SHELL"
+
+echo
+echo "C) Display the home directory"
+echo "Home directory is: $HOME"
+
+echo
+echo "Experiment completed successfully."
