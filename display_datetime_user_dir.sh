7038a506764a6e71ca8c384bc94bec5ee75cdf9c
Kevin A Dobbin <94518887+KEVINADOBBIN@users.noreply.github.com>
2026-01-08 12:23:15 +0530

    Add files via upload

---
+++ display_datetime_user_dir.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "------------------------------------"
+echo "EXPERIMENT NO: 13"
+echo "DISPLAY DATE, TIME, USERNAME, DIRECTORY"
+echo "------------------------------------"
+
+echo
+echo "1. Current date and time:"
+date
+
+echo
+echo "2. Username:"
+whoami
+
+echo
+echo "3. Current working directory:"
+pwd
+
+echo
+echo "Experiment completed successfully."
